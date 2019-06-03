b98f376ef6461d3f16f3d687525502f3c6b677ba
Aarjansingh <50768101+Aarjansingh@users.noreply.github.com>
2019-06-02 23:14:39 -0700

    Add files via upload

---
+++ biconditional.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+char C1,C2;
+printf("Enter T for true condition and F for false condition\n");
+scanf("%c %c",C1,C2 );
+if(C1=C2)
+printf("T");
+else
+    printf("F");
+    return 0;
+}
+
